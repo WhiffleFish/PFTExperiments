e5c43146d565471569e9419a57927344e241cd71
WhiffleFish <tylertronics@gmail.com>
2021-05-24 17:51:09 -0600

    POMCPOW opt complete

---
+++ util/restore.jl
@@ -0,0 +1,13 @@
+using JLD2
+
+"""
+Hyperopt macro stores objective as anonymous function, which breaks save with JLD2
+-> Restored version builds everything but field :objective
+-> :objective filed inconsequential when looking at data so replace with x->0
+"""
+function RestorHopt(path)
+    d = JLD2.load(path)
+    ho = first(values(d))
+    h = Hyperoptimizer([getfield(ho,f) for f in fieldnames(typeof(ho))]..., x->0)
+    return h
+end
