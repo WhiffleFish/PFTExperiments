270a81624d37d02ab27e7274e9bf7f37eebd5cbf
WhiffleFish <tylertronics@gmail.com>
2022-04-09 11:57:13 -0600

    LaserTag,SubHunt AdaOPS data

--- final/AdaOPS.jl
+++ final/AdaOPS.jl
@@ -18,3 +18,43 @@ p_final = joinpath(LIGHTDARK_DATA_PATH, "compare_2022_04_08.csv")
 
 b = BenchmarkSummary(p_final)
 COE.plot_data(b)
+
+##
+
+p1 = joinpath(LASERTAG_DATA_PATH, "compare_2021_10_03.csv")
+p2 = joinpath(LASERTAG_DATA_PATH, "AdaOPS_2022_04_09.csv")
+
+b = BenchmarkSummary(p1)
+COE.plot_data(b)
+
+
+
+df = COE.combinedf(p1,["POMCPOW", "PFTDPW", "SparsePFT"], p2, ["AdaOPS"])
+
+using CSV
+CSV.write("compare_2022_04_09.csv",df)
+
+p_final = joinpath(LASERTAG_DATA_PATH, "compare_2022_04_09.csv")
+
+b = BenchmarkSummary(p_final)
+COE.plot_data(b)
+
+##
+
+p1 = joinpath(SUBHUNT_DATA_PATH, "compare_2021_09_30.csv")
+p2 = joinpath(SUBHUNT_DATA_PATH, "AdaOPS_2022_04_08.csv")
+
+b = BenchmarkSummary(p2)
+COE.plot_data(b)
+
+
+
+df = COE.combinedf(p1,["POMCPOW", "PFTDPW", "SparsePFT"], p2, ["AdaOPS"])
+
+using CSV
+CSV.write("compare_2022_04_09.csv",df)
+
+p_final = joinpath(SUBHUNT_DATA_PATH, "compare_2022_04_09.csv")
+
+b = BenchmarkSummary(p_final)
+COE.plot_data(b)
