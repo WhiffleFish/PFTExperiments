92dd1a01eb1a6f89f6bf9202932df0e0a5b401ab
WhiffleFish <tylertronics@gmail.com>
2022-10-28 18:25:20 -0600

    plots that actually have TNR font

--- init.jl
+++ init.jl
@@ -5,6 +5,15 @@ using Pkg
 Pkg.activate(@__DIR__)
 
 try
+    using PFTBenchmarks
+    @info "✓ PFTBenchmarks"
+catch ArgumentError
+    @info "PFTBenchmarks not found - installing"
+    try Pkg.rm("PFTBenchmarks") catch end
+    Pkg.develop(path = joinpath(@__DIR__, "PFTBenchmarks"))
+end
+
+try
     using PFTPlots
     @info "✓ PFTPlots"
 catch ArgumentError
